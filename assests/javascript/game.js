66215133bed0f9e36095dfffe675a7aade42dcf7
BlackAce <alfredo.gon195@gmail.com>
2019-08-29 01:54:40 -0700

    added the radom function for the computer to make its guess and function for guesses left

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -30,7 +30,7 @@ var computerChoices = [
 
 /* var for starting score of game of zero */
 
-var wins =0;
+var wins = 0;
 
 var losses = 0;
 
@@ -39,4 +39,23 @@ var guessesLeft = 9;
 var letterUser = [];
 
 var eachOfLetters = null;
+
+/* make var computerGuess = computerChoices array */
+
+var computerGuess = computerChoices[Math.random() * computerChoices.length];
+console.log(computerGuess);
+
+/* make functions */
+
+function countGuessesLeft() {
+  document.querySelector("guessesLeft").innerText =
+    "Guesses Left" + guessesLeft;
+}
+
+/* functions for when users takes a guess */
+// document.onkeyup = function(event) {
+    // guessesLeft--;
+    
+    // var userGuess = (event.keyCode).toLowerCase();
+// } 
 
