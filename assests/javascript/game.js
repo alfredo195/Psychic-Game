29a520c624b26bb8a19ec953e6de9464193cea64
BlackAce <alfredo.gon195@gmail.com>
2019-08-29 00:44:56 -0700

    created var for computer choices along with var for place holers for beging of score borad

---
+++ assests/javascript/game.js
@@ -0,0 +1,42 @@
+/* var to hold values that the computer can pick from */
+var computerChoices = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+/* var for starting score of game of zero */
+
+var wins =0;
+
+var losses = 0;
+
+var guessesLeft = 9;
+
+var letterUser = [];
+
+var eachOfLetters = null;
+
