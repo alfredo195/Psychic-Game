31def8ac0391b4e81304fb396a1d720e89b04492
BlackAce <alfredo.gon195@gmail.com>
2019-08-30 00:03:23 -0700

    finished functions and if/if else/ else statments in js, linked images back ground

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -42,20 +42,46 @@ var eachOfLetters = null;
 
 /* make var computerGuess = computerChoices array */
 
-var computerGuess = computerChoices[Math.random() * computerChoices.length];
-console.log(computerGuess);
+var computerGuess =
+  computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 /* make functions */
 
 function countGuessesLeft() {
-  document.querySelector("guessesLeft").innerText =
+  document.querySelector("guessesLeft").innerText=
     "Guesses Left" + guessesLeft;
 }
 
 /* functions for when users takes a guess */
-// document.onkeyup = function(event) {
-    // guessesLeft--;
-    
-    // var userGuess = (event.keyCode).toLowerCase();
-// } 
+function farUserGuesses() {
+  document.querySelector("#letter").innerText = "Your Guesses so far: " + letterUser.join(" ");
+}
+countGuessesLeft();
+/* function to reset */
+var restart = function() {
+  guessesLeft = 9;
+  letterUser = [];
+  var computerGuess =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
 
+// When the user presses a key, it will run the following function..
+document.onkeyup = function(event) {
+  guessesLeft--;
+
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  letterUser.push(userGuess);
+  countGuessesLeft();
+  farUserGuesses();
+
+  if (userGuess === computerGuess) {
+    wins++;
+    document.querySelector("#wins").innerText = "Wins: " + wins;
+    restart();
+  } else if (guessesLeft === 0) {
+    losses++;
+    document.querySelector("#lose").innerText = "Loses: " + losses;
+    restart();
+  }
+};
